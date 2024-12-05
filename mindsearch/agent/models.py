8c17431979b0330b585e729eebf5ff27df5c922e
wangjuan012 <474990945@qq.com>
2024-12-05 10:43:16 +0800

    Update models.py

--- mindsearch/agent/models.py
+++ mindsearch/agent/models.py
@@ -78,7 +78,7 @@ qwen = dict(
 internlm_silicon = dict(
     type=GPTAPI,
     model_type="internlm/internlm2_5-7b-chat",
-    key=os.environ.get("SILICON_API_KEY", "YOUR SILICON API KEY"),
+    key=os.environ.get("SILICON_API_KEY", "sk-omqttmmonezfzngftpxsbuhhkezpnhlaerfydlbrcntllvdl"),
     api_base="https://api.siliconflow.cn/v1/chat/completions",
     meta_template=[
         dict(role="system", api_role="system"),
